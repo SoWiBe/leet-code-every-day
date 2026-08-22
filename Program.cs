3bee20e0fca9640926c8761022e516482eddc5d4
Aleksey <aleksey.dantist@gmail.com>
2026-08-23 01:33:53 +0600

    feat: 3. Longest Substring Without Repeating Characters

--- Program.cs
+++ Program.cs
@@ -1,4 +1,8 @@
 ﻿using LeetApp.subj.August.Session22;
+using LeetApp.subj.August.Session23;
 
-Session22 session = new Session22();
-Console.WriteLine(session.CheckInclusion("acb", "eidbaooo"));
+// Session22 session = new Session22();
+// Console.WriteLine(session.CheckInclusion("acb", "eidbaooo"));
+
+Session23 session23 = new Session23();
+Console.WriteLine(session23.LengthOfLongestSubstring("abba"));
