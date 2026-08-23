34fb61ea390e975d21ffc560d5adfadf3ef159de
Aleksey <aleksey.dantist@gmail.com>
2026-08-23 22:34:45 +0600

    feat: 3sum

--- subj/August/Session23/Session23.cs
+++ subj/August/Session23/Session23.cs
@@ -64,4 +64,39 @@ public class Session23
 
         return [];
     }
+    
+    /// <summary>
+    /// 15. 3Sum
+    /// </summary>
+    /// <param name="nums"></param>
+    /// <returns></returns>
+    public IList<IList<int>> ThreeSum(int[] nums) 
+    {
+        Array.Sort(nums);
+        List<IList<int>> result = [];
+
+        for (var i = 0; i < nums.Length - 2; i++)
+        {
+            if (i > 0 && nums[i] == nums[i - 1]) continue;
+            var left = i + 1;
+            var right = nums.Length - 1;
+
+            while (left < right)
+            {
+                var sum = nums[i] + nums[left] + nums[right];
+                if (sum == 0)
+                {
+                    result.Add([nums[i], nums[left], nums[right]]);
+                    while (left < right && nums[left] == nums[left + 1]) left++;
+                    while (left < right && nums[right] == nums[right - 1]) right--;
+                    left++;
+                    right--;
+                }
+                else if (sum < 0) left++;
+                else right--;
+            }
+        }
+
+        return result;
+    }
 }
